6243cb09d59e7103ee973c31e68e9cabdd4fa821
matthewpaulmcgowan <matthewpaulmcgowan@gmail.com>
2015-11-22 22:53:28 +0000

    Done.

--- lib/project_euler_factorial_digit_sum.rb
+++ lib/project_euler_factorial_digit_sum.rb
@@ -1,1 +1,27 @@
 # code your solution here
+def factorial(number)
+a=[]
+  b=number
+  while b>0
+    a.push(b)
+    b=b-1
+  end
+
+  c=a.inject(:*)
+  return c
+end
+
+def sum_of_digits(number)
+  q=number.to_s.split('')
+  y=q.map! do |i| i.to_i
+  end
+  z=y.inject(:+)
+  return z
+end
+
+def factorial_digit_sum(number)
+  answer=sum_of_digits(factorial(number))
+  return answer
+
+
+end
